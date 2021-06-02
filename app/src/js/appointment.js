352b0d66301c1ed50f197399c197a8dd1cbfe1f3
kmg2015 <72024847+kmg2015@users.noreply.github.com>
2021-06-02 12:26:13 +0530

    Update appointment.js

--- app/src/js/appointment.js
+++ app/src/js/appointment.js
@@ -1,1 +1,55 @@
 
+(function(window){
+  window.extractAppointment = function() {
+    var res = $.Deferred();
+
+    function onError() {
+      console.log('Loading Appt error', arguments);
+      res.reject();
+    }
+
+    function onReady(smart)  {
+      console.log('smart',smart);
+        var user = smart.user;        
+        var pt = user.read();
+        var appt = smart.api.fetchAll({
+                    type: 'Appointment',
+                    query: {
+                      practitioner: smart.tokenResponse.user,
+                      date: 2021
+                    }
+                  });
+        $.when(pt, appt).fail(onError);
+        $.when(pt, appt).done(function(user, appt) {
+          console.log('appt++++++++++++++++++',appt);
+          var p = defaultAppt();
+          p.apptStatus = appt[0].status;
+          p.description = appt[0].description;
+          p.minutesDuration = appt[0].minutesDuration;
+          res.resolve(p);
+        });
+    }
+
+    FHIR.oauth2.ready(onReady, onError);
+    return res.promise();
+
+  };
+
+  function defaultAppt(){
+    return {
+      apptStatus: {value: ''},
+      description: {value: ''},
+      minutesDuration: {value: ''}
+    };
+  }
+
+  window.drawApptVisualization = function(p) {
+    console.log('p+++++++++++++++++++',p);
+    $('#holder').show();
+    $('#loading').hide();    
+    $('#apptStatus').html(p.apptStatus);
+    $('#description').html(p.description);
+    $('#minutesDuration').html(p.minutesDuration);
+  };
+
+})(window);
